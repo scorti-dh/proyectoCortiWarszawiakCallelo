dcaa83436d591dc0c917896c83a73b9895d55350
scorti-dh <scortimaderna@udesa.edu.ar>
2026-06-08 02:50:30 -0300

    Hice una maqueta minima del CrearPost

--- src/screens/CrearPost/CrearPost.js
+++ src/screens/CrearPost/CrearPost.js
@@ -1,9 +1,21 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, Pressable, Image, StyleSheet } from 'react-native'
 
 function CrearPost() {
+    const [desc, setDesc] = useState('')
+    const [img, setImg] = useState('')
+
+
+
     return (
         <View>
-            <Text>Esto es Crear Posts</Text>
+            <Text style={styles.label}>Crea tu post</Text>
+            <TextInput
+                      style={styles.field}
+                      placeholder="Ingresa la imagen"
+                      onChangeText={(text) => setUsername(text)}
+                      value={username}
+                    />
         </View>
     )
 }
